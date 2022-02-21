b51a0186b6286fa07309c126c1fbd5dd7d29c93a
lilly <lilly1987@naver.com>
2022-02-22 08:27:42 +0900

    .

--- COM3D2.colorPresetNum.Plugin/ColorPresetNum.cs
+++ COM3D2.colorPresetNum.Plugin/ColorPresetNum.cs
@@ -19,7 +19,9 @@ namespace COM3D2.colorPresetNum.Plugin
         public const string PLAGIN_VERSION = "22.2.22";
         public const string PLAGIN_FULL_NAME = "COM3D2.colorPresetNum.Plugin";
     }
-
+    /// <summary>
+    /// 2.0  2.5 공용
+    /// </summary>
     [BepInPlugin(MyAttribute.PLAGIN_FULL_NAME, MyAttribute.PLAGIN_NAME, MyAttribute.PLAGIN_VERSION)]// 버전 규칙 잇음. 반드시 2~4개의 숫자구성으로 해야함. 미준수시 못읽어들임
     //[BepInPlugin("COM3D2.Sample.Plugin", "COM3D2.Sample.Plugin", "21.6.6")]// 버전 규칙 잇음. 반드시 2~4개의 숫자구성으로 해야함. 미준수시 못읽어들임
     //[BepInProcess("COM3D2x64.exe")]
